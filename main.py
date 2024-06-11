2b2e97e40638cd9d347641f60ae2d75df06d43b1
Leul Getaneh <60969133+miraj-code@users.noreply.github.com>
2024-06-11 11:50:39 +0300

    Update main.py
    Added comments to explain the code and removed unnecessary blank lines.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import the necessary libraries for the program
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -9,45 +10,60 @@ import torch.nn as nn
 
 from model import model
 
+# Load the path of the dataset from the environment variable
 load_dotenv()
 path = os.getenv('DATASET_PATH')
+
+# Read the csv dataset file to get the necessary features
 data = pd.read_csv(path)
 
+# Create a dictionary for the output layer to replace the Characters to Numbers. It's used for later to convert the array to tensors.
+# The dictionary is in the format of {diagnosis: id} 
 diagnosis = sorted(list(set(data['Diagnosis'])))
 diagnosis_dict = {}
 for i in range(len(diagnosis)):
     diagnosis_dict[diagnosis[i]] = i
 
+# Separate the input layers (are input features - X) and the output layers (y)
 X = data.drop('Diagnosis', axis = 1)
 y = data['Diagnosis']
 
+# Replace the rows of y (which are Char type) to Number type
 for i in range(len(y)):
     for key, value in diagnosis_dict.items():
         if y[i] == key:
             y = y.replace(y[i], value)
 
+# Convert X and y to array
 X = X.values
 y = y.values
 
+# Split X and y to train and test datasets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=61)
 
+# Convert the X and y arrays to Tensor for the model to process them correctly
 X_train = torch.FloatTensor(X_train)
 X_test = torch.FloatTensor(X_test)
-
 y_train = torch.LongTensor(y_train)
 y_test = torch.LongTensor(y_test)
 
+# Set the Loss function and Optimizer function
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters())
 
+# Set a value for the number of epoches
 epoches = 4000
 
+# Train the model iterating through the number of epoches
 for i in range(epoches):
+    # Forward the training dataset through the layers in the model
     y_pred = model.forward(X_train)
+    # Calculate the loss
     loss = criterion(y_pred, y_train) 
 
     print(f'Epoch {i+1} | Loss: {loss}')
 
+    # Back propagate the loss to further train the model
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
