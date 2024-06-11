441ff57d591bb49b78ec8a38a96b2e29c707f0b8
Leul Getaneh <60969133+miraj-code@users.noreply.github.com>
2024-06-11 11:57:51 +0300

    Update model.py
    Added comments to explain the code.

--- model.py
+++ model.py
@@ -1,15 +1,21 @@
+# Import the necessary liraries to build the model
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Create a class for the model
 class Model(nn.Module):
+    # Initialize the layers of the model. The model has 1 input layer (with 14 neurons), 
+    # 3 hidden layers (each with 15 neurons), and 1 output layer (with 9 neurons)
     def __init__(self, in_features = 14, h1 = 15, h2 = 15, h3 = 15, out_features = 9):
         super().__init__()
+        # Create 5 fully connected layers
         self.fc1 = nn.Linear(in_features, h1)
         self.fc2 = nn.Linear(h1, h2)
         self.fc3 = nn.Linear(h2, h3)
         self.out = nn.Linear(h3, out_features)
 
+    # Forward function to pass/forward data through the fully connected layers
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -17,6 +23,9 @@ class Model(nn.Module):
         x = self.out(x)
 
         return x
-    
+
+# Create a manual seed for randomization
 torch.manual_seed(61)
+
+# Initialize the model
 model = Model()
